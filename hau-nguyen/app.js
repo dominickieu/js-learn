14e5f60ae8789db726578680ef3927be88c223a5
T91 GAMING CENTER <qdominickieu@gmail.com>
2024-07-10 08:14:42 +0700

    init prj

---
+++ hau-nguyen/app.js
@@ -0,0 +1,78 @@
+/**
+ * - use strict
+ *
+ * - comments
+ *
+ * - variables: const/let/var
+ *
+ * - operators: toán tử toán học, toán tử gán, so sánh, unary, 3 ngôi, logic
+ *
+ * - data types
+ *
+ * - function
+ *
+ *
+ */
+
+/**
+ * BOOLEAN
+ * true and false
+ *
+ * - type conversion and type coersion: ép kiểu và ép kiểu tự động
+ *
+ * - truthy and falsy
+ * falsy values: false, 0, -0, NaN, null, undefined, '', "". ``
+ *
+ * - logical operators: &&, ||, !
+ *
+ * - if else
+ *
+ * - switch case
+ *
+ *
+ */
+
+/**
+ * NUMBER
+ *
+ * - Number object
+ *
+ * - Math object
+ *
+ * - Các hàm làm tròn số
+ */
+
+/**
+ * STRING
+ *
+ * - ``
+ *
+ * - Các hàm với string: substring, replace, split, join, capitalize, includes, truncate
+ */
+
+/**
+ * OBJECT
+ *
+ * - key value
+ *
+ * - tham chiếu và tham trị: tham chiếu
+ * value type lưu trữ giá trị của biến
+ * ref type lưu trữ địa chỉ vào biến
+ *
+ * - Các thao tác: shorthand, destructuring, key in object, clone object
+ */
+
+/**
+ * ARRAY
+ *
+ * - Array.isArray, Array.from
+ *
+ * - destructuring
+ *
+ * - clone
+ *
+ * - loop
+ *
+ * - Các methods: push, unshift, includes, some,
+ * find, map, filter, sort, reduce
+ */
